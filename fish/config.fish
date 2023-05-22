b29dff4ac2cbb73fd2e1ff5b5fca5d3544ab7a87
Timothy <yexiaozhou2003@gmail.com>
2023-05-22 21:40:18 +0800

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,25 @@
-alias gs="git status"
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set PATH /root/nvim/bin $PATH
+set PATH /root/go/bin $PATH
+set GOPATH /root/workspace $GOPATH
+set GOPROXY direct $GOPROXY
+
 starship init fish | source
+alias gs="git status"
+alias gsm="git summary"
+alias ga='git add'
+alias gd='git diff'
+alias gf='git fetch'
+alias grv='git remote -v'
+alias grb='git rebase'
+alias gbr='git branch'
+alias gpl="git pull"
+alias gps="git push"
+alias gco="git checkout"
+alias gl="git log"
+alias gc="git commit -m"
+alias gm="git merge"
+alias vim="nvim"
